dc02e288e47ea69481171050dcaa3cdb94e7f101
fummicc1 <fumiya.tennis1@gmail.com>
2020-06-03 19:39:21 +0900

    add operator for AccurateType.

--- SimpleRoulette/Source/Internal/Operator.swift
+++ SimpleRoulette/Source/Internal/Operator.swift
@@ -13,3 +13,19 @@ infix operator ~=~
 func ~=~<Accuracy: AccuracyType>( rhs: inout Accuracy, lhs: Accuracy) -> Bool where Accuracy.Value == Double {
     rhs.subtract(lhs, mutate: false).truncatingRemainder(dividingBy: Double.pi * 2) == 0
 }
+
+func +<Accuracy: AccuracyType>(rhs: inout Accuracy, lhs: Accuracy) -> Accuracy {
+    return rhs.add(lhs, mutate: false)
+}
+
+func -<Accuracy: AccuracyType>(rhs: inout Accuracy, lhs: Accuracy) -> Accuracy.Value {
+    return rhs.subtract(lhs, mutate: false)
+}
+
+func *<Accuracy: AccuracyType>(rhs: inout Accuracy, lhs: Accuracy) -> Accuracy.Value {
+    return rhs.multiply(with: lhs, mutate: false)
+}
+
+func /<Accuracy: AccuracyType>(rhs: inout Accuracy, lhs: Accuracy) -> Accuracy.Value {
+    return rhs.divide(by: lhs, mutate: false)
+}
